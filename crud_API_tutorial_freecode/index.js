7df7e94a9465f10b8f499720ef1c0f1148b5b002
Marco Andrade <marcoantonio.andrade05@gmail.com>
2024-09-05 20:02:25 -0300

    Conexão com MongoDB e criação de Modelos.

--- crud_API_tutorial_freecode/index.js
+++ crud_API_tutorial_freecode/index.js
@@ -1,1 +1,18 @@
-console.log("teste")
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+app.get('/', (req, res) => {
+     res.send("Hello from node API Server");
+});
+
+mongoose.connect("mongodb+srv://marcoantonioandrade05:32663112Ma@crudapitutorial.llwze.mongodb.net/Node-API?retryWrites=true&w=majority&appName=CRUDAPITUTORIAL")
+.then(() => {
+    console.log("Connected to database!")
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
+})
+.catch(() => {
+    console.log("connection failed!")
+})
