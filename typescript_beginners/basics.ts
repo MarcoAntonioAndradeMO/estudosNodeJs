d99bcfb16a48d248a9a15dc0f4c6ab102e4fbddf
Marco Andrade <marcoantonio.andrade05@gmail.com>
2024-09-05 10:29:48 -0300

    Função Void

--- typescript_beginners/basics.ts
+++ typescript_beginners/basics.ts
@@ -47,3 +47,38 @@ let age: number[] = [100, 101]
 let people: Person[] = [person1, person2]
 
 // let people: Array<Person>[] = [person1,person2]
+
+// Literal Types
+let myName1 = "Marco"
+let myName2: "Marco" | "Mark" // => Só aceitará esses valores
+
+//Unions
+type UserRole = "admin" | "member" | "guest" // => Similar a ENUMS 
+
+type User = {
+    username: string,
+    role: "admin" | "member" | "guest"
+}
+
+let userRole: UserRole = "member"
+
+const users: User[] = [
+    {username: "john_doe", role: "member"},
+    {username: "jane_doe", role: "admin"},
+    {username: "guest_user", role: "guest"}
+]
+
+function fetchUserDetails(username: string): User {
+    const user = users.find(user => user.username === username)
+    if (!user) {
+        throw new Error(`User with username: ${username} not found`)
+    }
+    return user
+}
+// a função deixa explícito que o tipo de retorno e User, facilitando o Debug do código 
+
+// Função Void x Undefined
+// void => indica que a função não deve retornar um valor
+// O tipo void é bastante utilizado com callbacks e funções de evento
+// Usado para funções que não retornam valor 
+// undefined => é um valor literal que pode ser retornado ou usado explicitamente
--- typescript_beginners/index.ts
+++ typescript_beginners/index.ts
@@ -1,4 +1,5 @@
 type Pizza ={
+    id: number,
     name: string,
     price: number
 }
@@ -6,14 +7,14 @@ type Pizza ={
 type Order = {
     id: number,
     pizza: Pizza,
-    status: string
+    status: "ordered" | "completed",
 }
 
-const menu = [
-    {name: "Margherita", price: 8},
-    {name: "Pepperoni", price: 10},
-    {name: "Hawaiian", price: 10},
-    {name: "Veggie", price: 9},
+const menu: Pizza[] = [
+    {id: 1,name: "Margherita", price: 8},
+    {id: 2,name: "Pepperoni", price: 10},
+    {id: 3,name: "Hawaiian", price: 10},
+    {id: 4,name: "Veggie", price: 9},
 ]
 
 let cashInRegister: number = 100;
@@ -31,7 +32,7 @@ function placeOrder(pizzaName: string) {
         return
     }
     cashInRegister += selectedPizza.price
-    const newOrder = {id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
+    const newOrder: Order = {id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
     orderQueue.push(newOrder)
     return newOrder
 }
@@ -46,9 +47,19 @@ function completeOrder(orderId){1
     return order
 }
 
-addNewPizza({ name: "Chicken Bacon Ranch", price: 12})
-addNewPizza({ name: "BBQ Chicken", price: 12})
-addNewPizza({ name: "Spicy Sausage", price: 11})
+export function getPizzaDetail(identifier: string | number) {
+    if (typeof identifier === "string") {
+        return menu.find(pizza => pizza.name.toLowerCase() === identifier.toLowerCase())
+    } else if (typeof identifier === "number"){
+        return menu.find(pizza => pizza.id === identifier)
+    } else {
+        throw new TypeError("Parameter 'identifier' must be either a string or a number.")
+    }
+}
+
+addNewPizza({id: 5, name: "Chicken Bacon Ranch", price: 12})
+addNewPizza({id: 6, name: "BBQ Chicken", price: 12})
+addNewPizza({id: 7, name: "Spicy Sausage", price: 11})
 
 placeOrder("Chicken Bacon Ranch")
 completeOrder("1")
