699457b51289e9cdae9d8016622f2500257f9ddb
Marco Andrade <marcoantonio.andrade05@gmail.com>
2024-09-06 15:52:19 -0300

    Update API

--- crud_API_tutorial_freecode/index.js
+++ crud_API_tutorial_freecode/index.js
@@ -10,6 +10,7 @@ app.get('/', (req, res) => {
      res.send("Hello from node API Server");
 });
 
+// Adicionar produtos (Create)
 app.post('/api/products', async (req, res) => {
     try {
         const product = await Product.create(req.body);
@@ -19,13 +20,45 @@ app.post('/api/products', async (req, res) => {
     }
 });
 
-app.get('./api/products', async (req, res) => {
+//Listar todos os Produtos (Read)
+app.get('/api/products', async (req, res) => {
     try {
         const products = await Product.find({});
+        res.status(200).json(products);
     } catch (error) {
         res.status(500).json({message: error.message});
     }
 });
+
+// Selecionar Produto (Read Only)
+// Falta selecionar o ID
+app.get('/api/products/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.find(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+// Update
+app.put('/api/product/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const product = await Product.find(id, req.body);
+
+        if (!product) {
+            return res.status(404).json({message: "Product not found"})
+        }
+
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
 
 mongoose.connect("mongodb+srv://marcoantonioandrade05:32663112Ma@crudapitutorial.llwze.mongodb.net/Node-API?retryWrites=true&w=majority&appName=CRUDAPITUTORIAL")
 .then(() => {
