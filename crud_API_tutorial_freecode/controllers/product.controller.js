0c22207bf5f0d9a30f618209f04dc4c3cd8f9c08
Marco Andrade <marcoantonio.andrade05@gmail.com>
2024-09-08 11:47:17 -0300

    Crud controller

--- crud_API_tutorial_freecode/controllers/product.controller.js
+++ crud_API_tutorial_freecode/controllers/product.controller.js
@@ -22,7 +22,57 @@ const getProduct =  async (req, res) => {
     }
 }
 
+// Adicionar produtos (Create)
+const createProduct = async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+};
+
+// Update
+const updateProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const product = await Product.find(id, req.body);
+
+        if (!product) {
+            return res.status(404).json({message: "Product not found"})
+        }
+
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+};
+
+//Delete API
+const deleteProduct =  async (req, res) => {
+    try {
+        const { id } = req.params
+
+        const product = await Product.findByIdAndDelete(id);
+
+        if (!product) {
+            return res.status(404).json({message: "Product not found"})
+    
+        }
+
+        res.status(200).json({message: "Product deleted successfuly"})
+
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+};
+
 module.exports = {
     getProducts,
     getProduct,
+    createProduct,
+    updateProduct,
+    deleteProduct
 }
--- crud_API_tutorial_freecode/index.js
+++ crud_API_tutorial_freecode/index.js
@@ -14,65 +14,6 @@ app.get('/', (req, res) => {
      res.send("Hello from node API Server");
 });
 
-// Adicionar produtos (Create)
-app.post('/api/products', async (req, res) => {
-    try {
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
-
-
-app.get('/api/products/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.find(id);
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
-
-// Update
-app.put('/api/products/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-
-        const product = await Product.find(id, req.body);
-
-        if (!product) {
-            return res.status(404).json({message: "Product not found"})
-        }
-
-        const updatedProduct = await Product.findById(id);
-        res.status(200).json(updatedProduct);
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
-
-
-//Delete API
-app.delete('/api/products/:id', async (req, res) => {
-    try {
-        const { id } = req.params
-
-        const product = await Product.findByIdAndDelete(id);
-
-        if (!product) {
-            return res.status(404).json({message: "Product not found"})
-    
-        }
-
-        res.status(200).json({message: "Product deleted successfuly"})
-
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
-
 mongoose.connect("mongodb+srv://marcoantonioandrade05:32663112Ma@crudapitutorial.llwze.mongodb.net/Node-API?retryWrites=true&w=majority&appName=CRUDAPITUTORIAL")
 .then(() => {
     console.log("Connected to database!")
